744f926ffa51eefe47d6f9192aee8fb5c1094b7d
affu0042 <50651875+affu0042@users.noreply.github.com>
2022-02-20 22:40:18 +0530

    Update styles.js

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -7,4 +7,14 @@ export default makeStyles((theme) => ({
       display: 'block',
     },
   },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': { backgroundColor: alpha(theme.palette.common.white, 0.25) },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: { marginLeft: theme.spacing(3), width: 'auto' },
+  },
   
