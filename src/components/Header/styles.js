7a14a29bf650a079e54c9dad88b332c604f5b424
affu0042 <50651875+affu0042@users.noreply.github.com>
2022-02-20 22:41:22 +0530

    Update styles.js

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -17,4 +17,16 @@ export default makeStyles((theme) => ({
     width: '100%',
     [theme.breakpoints.up('sm')]: { marginLeft: theme.spacing(3), width: 'auto' },
   },
-  
+  searchIcon: {
+    padding: theme.spacing(0, 2), height: '100%', position: 'absolute', pointerEvents: 'none', display: 'flex', alignItems: 'center', justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0), paddingLeft: `calc(1em + ${theme.spacing(4)}px)`, transition: theme.transitions.create('width'), width: '100%', [theme.breakpoints.up('md')]: { width: '20ch' },
+  },
+  toolbar: {
+    display: 'flex', justifyContent: 'space-between',
+  },
+}));
