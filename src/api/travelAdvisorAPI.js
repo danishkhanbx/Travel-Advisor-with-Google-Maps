b239c4273dd36320d2509446e084497c0b8942ba
Mudasser012 <92203368+Mudasser012@users.noreply.github.com>
2022-02-21 20:34:54 +0530

    API key inserted

--- src/api/travelAdvisorAPI.js
+++ src/api/travelAdvisorAPI.js
@@ -4,13 +4,13 @@ export const getPlacesData = async (type, sw, ne) => {
   try {
     const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
       params: {
-        bl_latitude: sw.lat,
-        bl_longitude: sw.lng,
-        tr_longitude: ne.lng,
-        tr_latitude: ne.lat,
+        bl_latitude: '11.847676',
+        tr_latitude: '12.838442',
+        bl_longitude: '109.095887',
+        tr_longitude: '109.149359',,
       },
       headers: {
-        'x-rapidapi-key': process.env.REACT_APP_RAPID_API_TRAVEL_API_KEY,
+        'x-rapidapi-key': '8a3c715cbemsh7bd67df1c876464p11d2a1jsna213772475a6',
         'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
       },
     });
