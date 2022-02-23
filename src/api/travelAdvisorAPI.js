ca0e7bac524282599de22d1fb3d2f3baf7cc17e9
Mudasser012 <92203368+Mudasser012@users.noreply.github.com>
2022-02-23 19:12:59 +0530

    Update travelAdvisorAPI.js

--- src/api/travelAdvisorAPI.js
+++ src/api/travelAdvisorAPI.js
@@ -1,13 +1,14 @@
+/* eslint-disable consistent-return */
 import axios from 'axios';
 
 export const getPlacesData = async (type, sw, ne) => {
   try {
     const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
       params: {
-        bl_latitude: '11.847676',
-        tr_latitude: '12.838442',
-        bl_longitude: '109.095887',
-        tr_longitude: '109.149359',,
+        bl_latitude: 'sw.lat',
+        tr_latitude: 'sw.lng',
+        bl_longitude: 'ne.lng',
+        tr_longitude: 'ne.lat',,
       },
       headers: {
         'x-rapidapi-key': '',
@@ -16,6 +17,24 @@ export const getPlacesData = async (type, sw, ne) => {
     });
 
     return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    if (lat && lng) {
+      const { data } = await axios.get('https://community-open-weather-map.p.rapidapi.com/find', {
+        params: { lat, lon: lng },
+        headers: {
+          'x-rapidapi-key': process.env.REACT_APP_RAPID_API_WEATHER_API_KEY,
+          'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+        },
+      });
+
+      return data;
+    }
   } catch (error) {
     console.log(error);
   }
