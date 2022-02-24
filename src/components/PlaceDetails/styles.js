11c031cd3fbfd4071964bdfc5d95e6953f75cedb
affu0042 <50651875+affu0042@users.noreply.github.com>
2022-02-24 20:53:52 +0530

    Update styles.js

--- src/components/PlaceDetails/styles.js
+++ src/components/PlaceDetails/styles.js
@@ -4,3 +4,10 @@ export default makeStyles(() => ({
   chip: {
     margin: '5px 5px 5px 0',
   },
+  subtitle: {
+    display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginTop: '10px',
+  },
+  spacing: {
+    display: 'flex', alignItems: 'center', justifyContent: 'space-between',
+  },
+}));
