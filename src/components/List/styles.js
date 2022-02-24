d13748b415f201808a79cd17e7755ed9f186fb50
affu0042 <50651875+affu0042@users.noreply.github.com>
2022-02-24 20:43:28 +0530

    Update styles.js

---
+++ src/components/List/styles.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1), minWidth: 120, marginBottom: '30px',
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+  loading: {
+    height: '600px', display: 'flex', justifyContent: 'center', alignItems: 'center',
+  },
+  
