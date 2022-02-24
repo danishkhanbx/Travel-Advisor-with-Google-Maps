8464d9f3d8dba08e0fc43b8d72b2d687500212bf
Amaanshaikh1 <98114130+Amaanshaikh1@users.noreply.github.com>
2022-02-25 00:21:12 +0530

    Update List.jsx

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect, createRef } from 'react';
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
 
-const List = () => {
-    return (
-        <div>
-            <h1>List</h1>
-        </div>
-    );
-}
+import PlaceDetails from '../PlaceDetails/PlaceDetails';
+import useStyles from './styles.js';
 
-export default List;
+const List = ({ places, type, setType, rating, setRating, childClicked, isLoading }) => {
+  const [elRefs, setElRefs] = useState([]);
+  const classes = useStyles();
+
+  useEffect(() => {
+    setElRefs((refs) => Array(places.length).fill().map((_, i) => refs[i] || createRef()));
+  }, [places]);
+
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4">Food & Dining around you</Typography>
+      {isLoading ? (
+        <div className={classes.loading}>
