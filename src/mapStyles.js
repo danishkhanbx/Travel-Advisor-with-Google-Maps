0579dbf7faaba463dd8bb7ddcc3149a7ae12a0c3
affu0042 <50651875+affu0042@users.noreply.github.com>
2022-02-24 21:06:06 +0530

    Update mapStyles.js

---
+++ src/mapStyles.js
@@ -0,0 +1,19 @@
+export default [
+  {
+    featureType: 'all',
+    elementType: 'all',
+    stylers: [
+      {
+        saturation: '32',
+      },
+      {
+        lightness: '-3',
+      },
+      {
+        visibility: 'on',
+      },
+      {
+        weight: '1.18',
+      },
+    ],
+  },
