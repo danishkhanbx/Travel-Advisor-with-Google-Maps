7783679513a7ef712b66c27ac363f6c9ade621ef
affu0042 <50651875+affu0042@users.noreply.github.com>
2022-02-24 20:44:00 +0530

    Update styles.js

--- src/components/List/styles.js
+++ src/components/List/styles.js
@@ -10,4 +10,13 @@ export default makeStyles((theme) => ({
   loading: {
     height: '600px', display: 'flex', justifyContent: 'center', alignItems: 'center',
   },
-  
+  container: {
+    padding: '25px',
+  },
+  marginBottom: {
+    marginBottom: '30px',
+  },
+  list: {
+    height: '75vh', overflow: 'auto',
+  },
+}));
