d9cd0611c30ec978782cfa2d815cb935aed4d55f
affu0042 <50651875+affu0042@users.noreply.github.com>
2022-02-24 20:49:34 +0530

    Update styles.js

---
+++ src/components/Map/styles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  paper: {
+    padding: '10px', display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '100px',
+  },
+  mapContainer: {
+    height: '85vh', width: '100%',
+  },
